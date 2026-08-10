240d85ee61c6dd7f043b508f4664c54c6d87b8d2
injora1010-lang <injora1010@gmail.com>
2026-08-10 20:50:46 +0800

    Add files via upload

--- aplikasi.py
+++ aplikasi.py
@@ -189,12 +189,12 @@ if prompt_data:
    clean_prompt = re.sub(r'[\u200b-\u200d\ufeff\u200e\u200f]', '', clean_prompt).strip()
 
    # C. Jika setelah dibersihkan teks tidak kosong
-    if clean_prompt:
-        # Tampilkan pesan user
-        with st.chat_message("user", avatar="👤"):
-             st.markdown(clean_prompt)
-        
-             st.session_state.messages.append({"role": "user", "content": clean_prompt})
+   if clean_prompt:
+       # Tampilkan pesan user
+       with st.chat_message("user", avatar="👤"):
+           st.markdown(clean_prompt)
+ 
+       st.session_state.messages.append({"role": "user", "content": clean_prompt})
 
         # Proses jawaban AI
         with st.chat_message("assistant", avatar="🤖"):
