84c3fec1c2162c500bce162e19ddfb718ae90f5e
injora1010-lang <injora1010@gmail.com>
2026-08-10 20:35:03 +0800

    Add files via upload

--- aplikasi.py
+++ aplikasi.py
@@ -182,14 +182,14 @@ if prompt_data:
    raw_prompt = prompt_data.text
    uploaded_files = prompt_data.files
 
-      # A. Saring & buang karakter non-ASCII tersembunyi (\u200e dll)
-      clean_prompt = raw_prompt.encode("ascii", errors="ignore").decode("ascii")
+          # A. Saring & buang karakter non-ASCII tersembunyi (\u200e dll)
+          clean_prompt = raw_prompt.encode("ascii", errors="ignore").decode("ascii")
  
-      # B. Pembersihan ekstra dengan Regex untuk karakter formatting
-      clean_prompt = re.sub(r'[\u200b-\u200d\ufeff\u200e\u200f]', '', clean_prompt).strip()
+          # B. Pembersihan ekstra dengan Regex untuk karakter formatting
+          clean_prompt = re.sub(r'[\u200b-\u200d\ufeff\u200e\u200f]', '', clean_prompt).strip()
 
-      # C. Jika setelah dibersihkan teks tidak kosong
-      if clean_prompt:
+          # C. Jika setelah dibersihkan teks tidak kosong
+         if clean_prompt:
         # Tampilkan pesan user
         with st.chat_message("user", avatar="👤"):
              st.markdown(clean_prompt)
